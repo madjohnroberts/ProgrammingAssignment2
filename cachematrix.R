8bff94413f49e426ee9ee2608148cdcf67f5babc
Brian Sculac <bsculac@gmail.com>
2020-04-17 13:58:28 -0400

    1st draft of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Make a list that stores different parts of a "matrix object"
+## List stores fucntions that access the matrix and its inverse in the global env
+## Assumes the matrix is invertible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Makes a wierd object for storing a matrix and it's inverse
+  ## Args: 
+  ##    x (matrix): An invertible matrix 
+  ## Returns:
+  ##    list: Contains the set(), get(), set_inv(), and get_inv() methods
+  ## The methods set() and set_inv() will store to global the following variables:
+  ## set() stores its argument to "x" in global
+  ## set_inv() stores its argument to "inv" in global
+  ## The get() and get_inv() methods return "x" and "inv" respectively
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  set_inv <- function(inverse) inv <<- inverse
+  get_inv <- function() inv
+  return(list(set=set,
+              get=get,
+              set_inv=set_inv,
+              get_inv=get_inv
+              ))
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  ## Returns a matrix that is the inverse of 'x'
+  ## Checks if the inverse is in global before calculating the inverse 
+  ## Args:
+  ##    x (list): Weird "matrix object" consructed using "makeCacheMatrix"
+  ## Returns:
+  ##    matrix: inverse of the matrix stored in x
+  x_inv <- x$get_inv()
+  if(!is.null(x_inv)){
+    message("Fetching cached inverse matrix")
+    return(x_inv)
+  }
+  m <- x$get()
+  m_inv <- solve(m)
+  x$set_inv(m_inv)
+  return(m_inv)
 }
